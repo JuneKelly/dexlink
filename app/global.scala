b721b50d07f9b1a142aaa24b2c94293bf81b2687
Shane Kilkelly <shane@kilkelly.me>
2014-11-12 20:06:59 +0000

    seed user accounts

--- app/global.scala
+++ app/global.scala
@@ -1,15 +1,31 @@
 import play.api._
 import play.api.mvc._
+import services.user.UserAccountService
 
 
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
+    Lifecycle.seedUserAccounts()
     Logger.info("Dexlink has started...")
   }
 
   override def onStop(app: Application) {
     Logger.info("Dexlink shutdown...")
+  }
+
+}
+
+
+object Lifecycle {
+
+  def seedUserAccounts(): Unit = {
+    if(!UserAccountService.exists("admin@example.com")) {
+      UserAccountService.create("admin@example.com", "password")
+    }
+    if(!UserAccountService.exists("test@example.com")) {
+      UserAccountService.create("test@example.com", "password")
+    }
   }
 
 }
