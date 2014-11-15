c87b35f044deb6efe97594ce581e28f8023e1b21
Shane Kilkelly <shane@kilkelly.me>
2014-11-15 13:01:38 +0000

    formatting

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -11,32 +11,44 @@ import helpers.ViewContext
 object Login extends Controller {
 
   def login = Action { implicit request =>
-    Ok(views.html.login(ViewContext(), LoginForm.form))
+    Ok(views.html.login(ViewContext(),
+       LoginForm.form))
   }
 
   def loginPost = Action { implicit request =>
     LoginForm.form.bindFromRequest.fold(
+
       formWithErrors => {
-        Ok(views.html.login(ViewContext(), formWithErrors))
+        Ok(views.html.login(ViewContext(),
+           formWithErrors))
       },
+
       formData => {
         val userName = formData.email
         val password = formData.password
+
         UserAccountService.validateCredentials(userName, password) match {
+
           case Some(userID) =>
             Redirect(routes.Application.index)
               .flashing("success" -> s"Logged in ${userID}")
               .withSession("user" -> userID)
+
           case None =>
-            Ok(views.html.login(ViewContext(), LoginForm.form.withGlobalError("Login failed")))
+            Ok(views.html.login(ViewContext(),
+               LoginForm.form.withGlobalError("Login failed")))
         }
       }
+
     )
   }
 
   def logout = Action { implicit request =>
-    Redirect(routes.Application.index).flashing("success" -> "Logged out").withNewSession
+    Redirect(routes.Application.index)
+      .flashing("success" -> "Logged out")
+      .withNewSession
   }
+
 }
 
 object LoginForm {
@@ -48,8 +60,11 @@ object LoginForm {
 
   val form = Form(
     mapping(
-      "email" -> nonEmptyText(minLength = 2, maxLength = 128),
-      "password" -> nonEmptyText(minLength = 8))
+      "email" -> nonEmptyText(
+        minLength = 2,
+        maxLength = 128),
+      "password" -> nonEmptyText(
+        minLength = 8))
       (LoginForm.Data.apply)
       (LoginForm.Data.unapply)
   )
